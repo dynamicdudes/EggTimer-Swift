cb7b6d6e411dc1ab66d2885dc242a9e06a33bddb
Vishweshwaran <vishweshwaran@Vishweshwarans-MacBook-Pro.local>
2020-08-13 13:10:31 +0530

    Finished Full Project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    //Variable Declaration
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer  = Timer()
+    var alarmSound : AVAudioPlayer?
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed < totalTime{
+            secondsPassed += 1
 
+            let percentaageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentaageProgress
+        }
+        else{
+            timer.invalidate()
+            titleLabel.text = "Done"
+            playSoundCoding()
+        }
+    }
+    
+    func playSoundCoding(){
+        let path = Bundle.main.path(forResource: "alarm_sound.mp3", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do{
+            alarmSound = try AVAudioPlayer(contentsOf: url)
+            alarmSound?.play()
+        }
+        catch{
+            print("Can't play the sound...")
+        }
+    }
 
+    
 }
